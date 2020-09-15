101a2cfe74ec1f4e6f899085f52a1f94decb9414
Kjetil Pettersson <kjepet@Kjetils-MacBook-Pro-13.local>
2020-09-15 22:25:35 +0200

    2

--- js/norgeskart.js
+++ js/norgeskart.js
@@ -3,7 +3,8 @@ L.esri.basemapLayer('Topographic').addTo(map);
 L.esri.featureLayer({
     url: 'https://trdetestarcgi01.miljodirektoratet.no/arcgis/rest/services/interne_tjenester/kommuner_fylker/MapServer/0'
   }).addTo(map);
-  
+
   L.esri.featureLayer({
     url: 'https://trdetestarcgi01.miljodirektoratet.no/arcgis/rest/services/interne_tjenester/kommuner_fylker/MapServer/1'
   }).addTo(map);
+  
