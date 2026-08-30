846f7a71f5b84e29a093ff8d846553c19a894be2
Kjetil Pettersson <kjetil.pettersson@miljodir.no>
2026-08-31 01:26:42 +0200

    polygon

--- js/line_and_points_with_graphics.js
+++ js/line_and_points_with_graphics.js
@@ -1,9 +1,34 @@
 var map = L.map('map', { tap: false }).setView([63.4305, 10.3951], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
+
+var geocacheBilderLayer = L.esri.tiledMapLayer({
+  url: 'https://services.geodataonline.no/arcgis/rest/services/Geocache_WMAS_WGS84/GeocacheBilder/MapServer',
+  maxZoom: 19
+});
+
+var activeBackgroundLayer = osmLayer;
+
+var backgroundToggleControl = L.control({ position: 'topright' });
+backgroundToggleControl.onAdd = function () {
+  var button = L.DomUtil.create('button', 'leaflet-bar background-toggle-button');
+  button.type = 'button';
+  button.title = 'Bytt bakgrunnskart';
+  button.innerText = 'Bytt bakgrunnskart';
+
+  L.DomEvent.disableClickPropagation(button);
+  L.DomEvent.on(button, 'click', function () {
+    map.removeLayer(activeBackgroundLayer);
+    activeBackgroundLayer = activeBackgroundLayer === osmLayer ? geocacheBilderLayer : osmLayer;
+    activeBackgroundLayer.addTo(map);
+  });
+
+  return button;
+};
+backgroundToggleControl.addTo(map);
 
 // Source data is in EPSG:25833 (ETRS89 / UTM zone 33N), Leaflet needs WGS84.
 proj4.defs('EPSG:25833', '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
@@ -13,19 +38,67 @@ function toLatLng(coord) {
   return [wgs84[1], wgs84[0]];
 }
 
-var pointLayer = L.layerGroup().addTo(map);
-var lineLayer = L.layerGroup().addTo(map);
+function toLatLngRing(ring) {
+  return ring.map(toLatLng);
+}
+
+function buildPopupContent(props) {
+  var content = '<b>' + (props.popup_kort || '') + '</b>';
+  if (props.popup_lang) {
+    content += '<br>' + props.popup_lang;
+  }
+  return content;
+}
+
+// URL parameters: ?type=<value> filters features by their "type" property.
+// ?punkt=0|1, ?linje=0|1, ?omraade=0|1 toggle layer visibility (default shown).
+var urlParams = new URLSearchParams(window.location.search);
+var typeFilter = urlParams.get('type');
+
+function isLayerEnabled(name) {
+  return urlParams.get(name) !== '0';
+}
+
+function matchesTypeFilter(props) {
+  return !typeFilter || props.type === typeFilter;
+}
+
+// Expects the given property as a "#RRGGBB" hex string; falls back if missing/invalid.
+function resolveColor(props, propName, fallback) {
+  var hexColorPattern = /^#[0-9A-Fa-f]{6}$/;
+  return hexColorPattern.test(props[propName]) ? props[propName] : fallback;
+}
+
+var pointLayer = L.layerGroup();
+var lineLayer = L.layerGroup();
+var omraadeLayer = L.layerGroup();
+
+if (isLayerEnabled('punkt')) {
+  pointLayer.addTo(map);
+}
+if (isLayerEnabled('linje')) {
+  lineLayer.addTo(map);
+}
+if (isLayerEnabled('omraade')) {
+  omraadeLayer.addTo(map);
+}
 
 Promise.all([
   fetch('geojson/punkt.geojson').then(function (res) { return res.json(); }),
-  fetch('geojson/linje.geojson').then(function (res) { return res.json(); })
+  fetch('geojson/linje.geojson').then(function (res) { return res.json(); }),
+  fetch('geojson/omraade.geojson').then(function (res) { return res.json(); })
 ]).then(function (results) {
   var punktData = results[0];
   var linjeData = results[1];
+  var omraadeData = results[2];
 
   punktData.features.forEach(function (feature) {
-    var latlng = toLatLng(feature.geometry.coordinates);
     var props = feature.properties;
+    if (!matchesTypeFilter(props)) {
+      return;
+    }
+
+    var latlng = toLatLng(feature.geometry.coordinates);
 
     var icon = L.icon({
       iconUrl: props.ikon_url,
@@ -40,27 +113,59 @@ Promise.all([
       zIndexOffset: 1000,
       riseOnHover: true
     })
-      .bindPopup(props.beskrivelse || '')
+      .bindPopup(buildPopupContent(props))
       .addTo(pointLayer);
-
-    marker.on('click touchstart', function (e) {
-      L.DomEvent.stopPropagation(e);
-      marker.openPopup();
-    });
   });
 
   linjeData.features.forEach(function (feature) {
+    var props = feature.properties;
+    if (!matchesTypeFilter(props)) {
+      return;
+    }
+
     var latlngs = feature.geometry.coordinates.map(toLatLng);
     L.polyline(latlngs, {
-      color: '#D32F2F',
-      weight: 4,
+      color: '#e92020',
+      weight: 3,
+      dashArray: '5, 5',
       opacity: 0.9,
       lineJoin: 'round',
       interactive: false
     }).addTo(lineLayer);
   });
 
-  var bounds = L.featureGroup([pointLayer, lineLayer]).getBounds();
+  omraadeData.features.forEach(function (feature) {
+    var props = feature.properties;
+    if (!matchesTypeFilter(props)) {
+      return;
+    }
+
+    var latlngs = feature.geometry.coordinates.map(toLatLngRing);
+    var polygon = L.polygon(latlngs, {
+      color: resolveColor(props, 'linje_farge', '#333333'),
+      fillColor: resolveColor(props, 'farge', '#1E88E5'),
+      weight: 1,
+      fillOpacity: 0.5
+    })
+      .bindPopup(buildPopupContent(props))
+      .addTo(omraadeLayer);
+
+    if (props.gruppe) {
+      L.marker(polygon.getCenter(), {
+        icon: L.divIcon({
+          className: 'omraade-label',
+          html: props.gruppe,
+          iconSize: null
+        }),
+        interactive: false
+      }).addTo(omraadeLayer);
+    }
+  });
+
+  var allLayers = pointLayer.getLayers()
+    .concat(lineLayer.getLayers())
+    .concat(omraadeLayer.getLayers());
+  var bounds = L.featureGroup(allLayers).getBounds();
   if (bounds.isValid()) {
     map.fitBounds(bounds.pad(0.2));
   }
